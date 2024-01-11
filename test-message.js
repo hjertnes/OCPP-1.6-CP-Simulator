61bff20ceda97391700cc7ef1e7b0206a7baf83b
Eivind Hjertnes <me@hjertnes.me>
2024-01-11 09:51:26 +0100

    add test script

---
+++ test-message.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+// -*- coding: utf-8 -*-
+"use strict"
+
+const protocol = require("./lib/ocpp-protocol.js")
+
+protocol.readWSDLFiles();
+protocol.buildJSONSchemas();
+
+const values = {
+    "connectorId": 0,
+    "chargingProfile" : {
+        "chargingProfileId" : 1,
+        "chargingProfileKind" : "Absolute",
+        "chargingProfilePurpose" : "ChargePointMaxProfile",
+        "chargingSchedule" : {
+            "chargingRateUnit" : "W",
+            "chargingSchedulePeriod" : [ {
+                "limit" : 50000.0,
+                "startPeriod" : 0
+            } ],
+            "minChargingRate" : null,
+            "startSchedule" : "2023-11-30T16:00:50Z"
+        },
+        "recurrencyKind" : null,
+        "stackLevel" : 1,
+        "transactionId" : null
+    }
+};
+
+protocol.debugPayloadErrors(values, {version: 1.6, model: "cp", suffix: "Request", procName: "SetChargingProfile"})
